e8883e1a7982ffffe68cf49763017bfc016ae0e7
Lucas Viana Torres <lucas.lvtorres@gmail.com>
2025-02-17 19:22:35 -0300

    PUT - Configurando rotas crud tarefa

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -4,5 +4,9 @@ import TarefaController from "../controllers/TarefaController";
 const routers = Router();
 
 routers.get("/api/tarefa", TarefaController.get);
+routers.get("/api/tarefa/:id", TarefaController.getById);
+routers.post("/api/tarefa", TarefaController.post);
+routers.put("/api/tarefa/:id", TarefaController.put);
+routers.delete("/api/tarefa/:id", TarefaController.delete);
 
 export default routers;
